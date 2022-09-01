6aec9e7bdbc00e04b0865e622be237338235f772
“valeria100328” <maldonadoperezvaleria@gmail.com>
2022-09-01 00:55:36 -0500

    challengeBIT

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/components/home/home.component';
+import { HomeModule } from './modules/components/home.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
